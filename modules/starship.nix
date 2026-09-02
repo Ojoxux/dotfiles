777cffdfcd4ad794ba86d2dac025775ab8d8daef
Jou Okuyama <ojoxux@gmail.com>
2026-09-02 10:11:07 +0900

    カラースキーマを変えた

--- modules/starship.nix
+++ modules/starship.nix
@@ -6,13 +6,27 @@
     enableFishIntegration = true;
     settings = {
       "$schema" = "https://starship.rs/config-schema.json";
-      add_newline = true;
-      format = "[╭ {λ} ](bold green)\$directory\$battery\$all\$line_break\$character";
-      character = {
-        success_symbol = "[╰─>](bold green)";
-        error_symbol = "[x >](bold red)";
+      palette = "koda_monochrome";
+      palettes.koda_monochrome = {
+        fg = "#a6a6a6";
+        fg_bright = "#e0e0e0";
+        fg_dim = "#8a8a8a";
+        accent = "#c4c4c4";
+        alert = "#ffffff";
+        purple = "#a855f7";
       };
-      git_branch.format = "[$symbol$branch(:$remote_branch) ]($style)";
+      add_newline = true;
+      format = "[╭ ](bold fg_bright)[{](bold fg_dim)[λ](bold purple)[}](bold fg_dim)[ ](bold fg_bright)\$directory\$battery\$all\$line_break\$character";
+      character = {
+        success_symbol = "[╰─>](bold accent)";
+        error_symbol = "[x >](bold alert)";
+      };
+      directory.style = "bold fg";
+      git_branch = {
+        format = "[$symbol$branch(:$remote_branch) ]($style)";
+        style = "bold accent";
+      };
+      git_status.style = "bold fg_dim";
       time.disabled = true;
       aws.disabled = true;
       gcloud.disabled = true;
