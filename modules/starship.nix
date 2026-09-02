06a477aab4b299a020a1972cf7ae28996504c1cc
Jou Okuyama <ojoxux@gmail.com>
2026-09-02 09:57:08 +0900

    starshipをシンプルにした

--- modules/starship.nix
+++ modules/starship.nix
@@ -7,31 +7,13 @@
     settings = {
       "$schema" = "https://starship.rs/config-schema.json";
       add_newline = true;
-      format = "[╭{owo} ](bold green)\$username\$directory\$battery\$all\$line_break\$character";
+      format = "[╭{owo} ](bold green)\$directory\$battery\$all\$line_break\$character";
       character = {
         success_symbol = "[╰─>](bold green)";
         error_symbol = "[x >](bold red)";
       };
       git_branch.format = "[$symbol$branch(:$remote_branch) ]($style)";
-      git_status = {
-        format = "\$all_status\$ahead_behind ";
-        conflicted = "";
-        ahead = "";
-        behind = "";
-        diverged = "";
-        up_to_date = "[✓](bold green)";
-        untracked = "";
-        stashed = "";
-        modified = "🔥";
-        staged = "";
-        renamed = "";
-        deleted = "";
-      };
-      time = {
-        disabled = false;
-        format = "🕙 [$time]($style) ";
-        time_format = "%m/%d %R";
-      };
+      time.disabled = true;
       aws.disabled = true;
       gcloud.disabled = true;
       nodejs.disabled = true;
