acf461b75088851a05c501e2eaeea1ef2e4141f6
Ojoxux <j.okuyama.engineering@gmail.com>
2026-09-06 02:57:42 +0900

    cc足した

--- hosts/wsl.nix
+++ hosts/wsl.nix
@@ -15,6 +15,8 @@
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
   ];
 
+  nixpkgs.config.allowUnfree = true;
+
   nixpkgs.overlays = [
     (final: prev: {
       fetchurl = args:
